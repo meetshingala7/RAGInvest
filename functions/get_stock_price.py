ef06fc01b20736d56fbb28b83679340e3a0bc636
Meet Shingala <meetshingala2024@gmail.com>
2024-12-18 11:27:35 +0530

    I am able to add the table for stock code and also added a warning label

--- functions/get_stock_price.py
+++ functions/get_stock_price.py
@@ -16,29 +16,30 @@ def stock_price(stock_label = 'HDFCBANK'):
     }
 
     # Send GET request
-    response = requests.get(url, headers=headers)
-    print(response.status_code)
-    if response.status_code == 200:
-        # Parse the HTML content
-        # soup = BeautifulSoup(response.text, "html.parser")
-        soup = BeautifulSoup(response.content,"lxml")
+    try:
+        response = requests.get(url, headers=headers)
+        print(response.status_code)
+        # print(response.content)
+        if response.status_code == 200:
+            # Parse the HTML content
+            # soup = BeautifulSoup(response.text, "html.parser")
+            soup = BeautifulSoup(response.content,"lxml")
 
-        # Use XPath to select the desired element
-        # Attempt to find the element (this may not work for dynamic content)
-        ele_stock_pirce = soup.find("div", class_="YMlKec fxKbKc")
-        
-        figure = soup.find_all("div", class_="P6K39c")
-        names = soup.find_all("div", class_ = "mfs7Fc")
-        
-        if ele_stock_pirce or figure or names:
-            stock_price = ele_stock_pirce.text
-            figure = [i.text for i in figure]
-            names = [i.text for i in names]
+            # Attempt to find the element (this may not work for dynamic content)
+            ele_stock_pirce = soup.find("div", class_="YMlKec fxKbKc")
+            
+            figure = soup.find_all("div", class_="P6K39c")
+            names = soup.find_all("div", class_ = "mfs7Fc")
+            
+            if ele_stock_pirce or figure or names:
+                stock_price = ele_stock_pirce.text
+                figure = [i.text for i in figure]
+                names = [i.text for i in names]
 
-            return {'code': 200, "Stock Price": stock_price, "names": names, 'figure': figure}
-        else:
-            print("Failed to find the stock price. The content might be dynamically loaded.")
-    else:
-        print(f"Failed to retrieve the page. Status code: {response.status_code}")
-        return {'code': response.status_code, "Stock Price": None}
-
+                return {'code': 200, "Stock Price": stock_price, "names": names, 'figure': figure}
+            else:
+                print(f"Failed to retrieve the page. Status code: {response.status_code}")
+                return {'code': 310, "Stock Price": None}
+    except Exception as e:
+        print(e)
+        return {'code': 403,"Stock Price": None}
--- text_box.py
+++ text_box.py
@@ -29,42 +29,30 @@ if st.button('Enter'):
         st.write(f'You entered the stock code: {stock_code}')
         # Call the function and display its output
         function_output = stock_price(stock_code)
-        print(function_output)
-        if function_output['code'] == 200:
-            st.markdown(f"<h1>{function_output['Stock Price']}</h1>", unsafe_allow_html=True)
+        print('function_output', function_output)
+        if function_output['code'] == 200 and function_output['Stock Price']:
             # st.write(function_output)
-            previous_close = function_output['Previous Close']
+            try:
+                st.markdown(f"<h1>{function_output['Stock Price']}</h1>", unsafe_allow_html=True)
 
-            html = f"""
-            <div style="background-color:#f2f2f2;padding:20px;border-radius:10px;">
-            <h3>Stock Information</h3>
-            <table style="width:100%;">
-                <tr>
-                <td style="font-weight:bold;">PREVIOUS CLOSE</td>
-                <td style="text-align:right;">₹{previous_close}</td>
-                </tr>
-                <tr>
-                <td style="font-weight:bold;">DAY RANGE</td>
-                <td style="text-align:right;">₹{day_range[0]} - ₹{day_range[1]}</td>
-                </tr>
-                <tr>
-                <td style="font-weight:bold;">YEAR RANGE</td>
-                <td style="text-align:right;">₹{year_range[0]} - ₹{year_range[1]}</td>
-                </tr>
-                <tr>
-                <td style="font-weight:bold;">MARKET CAP</td>
-                <td style="text-align:right;">{market_cap} INR</td>
-                </tr>
-                <tr>
-                <td style="font-weight:bold;">AVG VOLUME</td>
-                <td style="text-align:right;">{avg_volume}M</td>
-                </tr>
-            </table>
-            </div>
-            """
-
-
-
+                names = function_output['names']
+                figures = function_output['figure']
+                if names and figures:
+                    html = '''<div style="background-color:#f2f2f2;padding:20px;border-radius:10px;">
+                    <h3>Stock Information</h3>
+                    <table style="width:100%;">'''
+                    for i in range(len(names)):
+                        html += f'''<tr>
+                            <td style="font-weight:bold;">{str(names[i])}</td>
+                            <td style="text-align:right;">{str(figures[i])}</td>
+                            </tr>                            '''
+                    html += """</table></div>"""
+                    print(html)
+                    st.markdown(html, unsafe_allow_html=True)
+            except Exception as e:
+                print(e)
+        if function_output['code'] == 310 and not function_output['Stock Price']:
+            st.warning('The stock code seems faulty. Please check!')
 
     else:
         st.write('Please enter a stock code.')
