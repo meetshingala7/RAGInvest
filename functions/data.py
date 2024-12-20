3f87e2b5d0673a6351b98cc5cb3d4110ce1f50a9
Meet Shingala <meetshingala2024@gmail.com>
2024-12-20 13:50:17 +0530

    Added Downloading Data and path with dates to calculate

---
+++ functions/data.py
@@ -0,0 +1,27 @@
+import yfinance as yf
+import pandas as pd
+import os
+
+
+def download_data(stock_symbol = 'RELIANCE', start_date = '2020-01-01', end_date = "2024-12-16"):
+    stock_symbol = stock_symbol.upper()
+    stock_symbol = f'{stock_symbol}.NS'
+    # Fetch historical data
+
+    directory_path = 'charting_functions/Data'
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    file_path = f'{stock_symbol}{start_date}{end_date}.parquet'
+    full_path = f'{directory_path}/{file_path}'
+
+    if not os.path.isfile(full_path):
+        data = yf.download(stock_symbol, start = start_date, end = end_date)
+        data.to_parquet(full_path)
+    
+    return full_path
+    
+    
+
+if __name__ == "__main__":
+    print(download_data(stock_symbol = 'HDFCBANK'))
---
+++ functions/dates.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+
+def calculate_dates(time_duration):
+    today = date.today()
+
+    if time_duration == "1D":
+        return_time = today - timedelta(days = 1)
+    
+    elif time_duration == '5D':
+        return_time = today - timedelta(days = 5)
+    
+    elif time_duration == '1M':
+        return_time = today - relativedelta(months=1)
+    
+    elif time_duration == '3M':
+        return_time = today - relativedelta(months=3)
+    
+    elif time_duration == '6M':
+        return_time = today - relativedelta(months=6)
+        
+    elif time_duration == '1Y':
+        return_time = today - relativedelta(years = 1)
+        
+    elif time_duration == '3Y':
+        return_time = today - relativedelta(years = 3)
+        
+    else:
+        return_time = today - relativedelta(years = 5)
+        
+    return return_time
+
+if __name__ == "__main__":
+    print(calculate_dates('1D'))
+    print(calculate_dates('5D'))
+    print(calculate_dates('1M'))
+    print(calculate_dates('3M'))
+    print(calculate_dates('6M'))
+    print(calculate_dates('1Y'))
+    print(calculate_dates('3Y'))
+    print(calculate_dates('5Y'))
