96511ca4cded2ee59d934ee69c131db4a9c24463
Meet Shingala <meetshingala2024@gmail.com>
2024-12-20 12:56:59 +0530

    Downloads the data between the dates with yahoo finance

--- charting_functions/finance_charts.py
+++ charting_functions/finance_charts.py
@@ -8,16 +8,21 @@ def download_data(stock_symbol = 'RELIANCE', start_date = '2020-01-01', end_date
     stock_symbol = stock_symbol.upper()
     stock_symbol = f'{stock_symbol}.NS'
     # Fetch historical data
-    data = yf.download(stock_symbol, start = start_date, end = end_date)
-
-    directory_path = 'charting_functions/Data'
-
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
+
+    directory_path = 'charting_functions/Data'
     
-    data.to_parquet(f'{directory_path}/{stock_symbol}{start_date}{end_date}.parquet')
-    # Display the data
-    print(data)
+    file_path = f'{stock_symbol}{start_date}{end_date}.parquet'
+    full_path = f'{directory_path}/{file_path}'
+
+    if not os.path.isfile(path):
+        data = yf.download(stock_symbol, start = start_date, end = end_date)
+        data.to_parquet(full_path)
+
+    return full_path
+    
+    
 
 if __name__ == "__main__":
     download_data(stock_symbol = 'HDFCBANK')
